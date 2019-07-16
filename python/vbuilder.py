7334af67248c7971854de0e0e4398ab82490a18d
Argos <argosofithica@gmail.com>
2019-07-16 19:20:26 -0400

    Add lookup table generation, add array generation

--- python/vbuilder.py
+++ python/vbuilder.py
@@ -5,6 +5,7 @@ Usage:
 	vbuilder.py register <name> <word_size> [--value=<v>]
 	vbuilder.py constant <name> <word_size> [--value=<v>]
 	vbuilder.py batch <name> <word_size> ( -r | -c ) <size>
+	vbuilder.py array <name> <word_size> ( -r | -c ) <size>
 	vbuilder.py constants <word_size>
 	vbuilder.py (-h | --help)
 	vbuilder.py --version
@@ -20,7 +21,7 @@ Options:
 from docopt import docopt
 
 types = list()
-version = "07142019"
+version = "07162019"
 
 class vartype():
 	short_form = ""
@@ -38,14 +39,6 @@ def build_types():
 	constant.long_form = "constant"
 	constant.is_writable = False
 	types.append(constant)
-	batch = vartype()
-	batch.short_form = "b"
-	batch.long_form = "batch"
-	types.append(batch)
-	all_constants = vartype()
-	all_constants.short_form = "ac"
-	all_constants.long_form = "all_constants"
-	types.append(all_constants)
 
 
 def verify_word_size(word):
@@ -71,15 +64,21 @@ def verify_batch_size(size):
 	assert(size > 0), "size must be positive integer"
 	return size
 
+def verify_array_size(size, word_size):
+	size = int(size)
+	word_size = int(word_size)
+	assert(size <= int(2**word_size))
+	return size
+
 def verify_value(value, word_size):
 	value = int(value)
-	max_int = int((2**word_size) / 2)
+	max_int = int(2**(word_size - 1))
 	assert(value < max_int ), "value is higher than maximum value"
 	assert(value >= -max_int ), "value is lower than minimum value"
 	return value
 
-get_bin = lambda x, n: format(x, 'b').zfill(n)
-
+def get_bin(value, bit_size):
+	return format(value, 'b').zfill(bit_size)
 
 def argcheck(args):
 	build_types()
@@ -112,20 +111,39 @@ def argcheck(args):
 		if args.get("-r"):
 			subtype = verify_type("register")
 			generate_batch(name, subtype, word_size, size)
+	elif args.get("array"):
+		name = args.get("<name>")
+		word_size = verify_word_size(args.get("<word_size>"))
+		size = verify_array_size(args.get("<size>"), args.get("<word_size>"))
+		if args.get("-c"):
+			subtype = verify_type("constant")
+			generate_array(name, subtype, word_size, size)
+		if args.get("-r"):
+			subtype = verify_type("register")
+			generate_array(name, subtype, word_size, size)
 
 def generate_all_constants(word_size):
-	max_int = int((2 ** word_size) / 2)
+	max_int = int(2 ** (word_size - 1))
 	constant_type = verify_type("constant")
 	for var in range( - max_int, max_int):
 		varname = str(var)
 		generate_variable(varname, constant_type, word_size, var)
 
 
+def generate_array(varname, subtype, word_size, size):
+	generate_batch(varname, subtype, word_size, size)
+
+	def get_string(var):
+		if var < size:
+			return 'alias a' + varname + '_ret r' + varname + str(var) + '; '
+		else:
+			return ''
+	generate_lookup_table('a' + varname, word_size, size, get_string)
+
 def generate_batch(varname, subtype, word_size, size):
 	for var in range(0, size):
 		bname = varname + str(var)
 		generate_variable(bname, subtype, word_size)
-
 
 
 def generate_variable(varname, type, word_size, value = 0):
@@ -154,9 +172,9 @@ def generate_variable(varname, type, word_size, value = 0):
 		
 	out += 'alias ' + prefix + '_init "alias alpha ' + prefix + '_is_a; alias beta ' + prefix + '_is_b; flag_order; alias ' + prefix + ' ' + prefix + 'cb; ' + prefix + 'cb"\n'
 	out += 'alias ' + prefix + ' ' + prefix + '_init\n\n'
-	
-	
-	
+
+
+
 	#bit values
 	bool_string = get_bin(value, word_size)
 	for x in range(1, word_size + 1):
@@ -195,6 +213,39 @@ def generate_variable(varname, type, word_size, value = 0):
 			out += 'alias ' + prefix + 'fr' + str(x) + ' "alias ' + prefix + 'b' + str(x) + ' bfalse"\n'
 		out += '\n'
 	print(out)
+
+def validate_true_branch(prefix, var, p, size):
+	new_branch = int(2**p) + var
+	if new_branch > size:
+		return prefix + 'fail' + str(p)
+	else:
+		return prefix + '1' + get_bin(var, p)
+
+
+def generate_lookup_table(prefix, word_size, size, custom_function):
+	out = ""
+	pointer = prefix + '_ptr'
+	p = 1
+	out += 'alias ' + prefix + '_ret\n'
+	out += 'alias ' + prefix + '_ptr\n'
+	out += 'alias ' + prefix + ' "alias btrue ' + prefix + '1; alias bfalse ' + prefix + '0; ' + pointer + '"\n\n'
+	while p != word_size:
+		for var in range(0, min(size, int(2**p))):
+			out += 'alias ' + prefix + get_bin(var, p) + ' "rcycle; alias btrue ' + validate_true_branch(prefix, var, p, size) + '; alias bfalse ' + prefix + '0' + get_bin(var, p) + '; ' + pointer + '"\n'
+		p += 1
+		out += '\n'
+	for var in range(0, size):
+		out += 'alias ' + prefix + get_bin(var, p) + ' "' + custom_function(var) + 'rcycle; deinit"\n'
+
+	#failure branches
+
+	for branch in range(1, word_size):
+		out += 'alias ' + prefix + 'fail' + str(branch) + ' "rcycle; ' + prefix + 'fail' + str(branch + 1) + '"\n'
+	out += 'alias ' + prefix + 'fail' + str(word_size)
+	print(out)
+
+
+
 
 if __name__ == '__main__':
 	args = docopt(__doc__, version=version)
