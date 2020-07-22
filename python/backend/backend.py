d0c0a64db990debd4ff8bfe9ff022b607d58ed50
ArgosOfIthica <ArgosOfIthica@gmail.com>
2020-07-22 00:25:30 -0400

    Remove leftover directories

--- python/backend/backend.py
+++
@@ -1,8 +0,0 @@
-import backend.generation.abstract_generation as gen
-import backend.emission.emission as emission
-
-
-def compile(global_object):
-	computation_tree = gen.compile(global_object)
-	config = emission.emit(computation_tree, global_object.universe)
-	return config
--- python/frontend/utility/utility.py
+++
@@ -1,8 +0,0 @@
-def token_is_name(token):
-	return token[0].isalpha()
-
-def token_is_numeric(token):
-	if token[0] == '-':
-		return token[1:].isnumeric()
-	else:
-		return token.isnumeric()
